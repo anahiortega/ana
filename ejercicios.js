259a5d16ef1eba0d4e356c4f469ae1ec263dc7fa
anahiortega <ortegaanahi135@gmail.com>
2024-08-10 18:46:00 -0500

    e

--- ejercicios.js
+++ ejercicios.js
@@ -1,6 +1,3 @@
 saluda=function(){
-    let cmpImagenSaludo;
-    cmpImagenSaludo=document.getElementById("imgSaludo")
-    cmpImagenSaludo.src="./graficos/4n1.gif"
     console.log("Hola, soy un saludo");
 }
